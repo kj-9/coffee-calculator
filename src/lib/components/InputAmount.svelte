19da88878cc74c9c3b63cb91de8ea6f59f608ba6
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-01 17:45:35 +0900

    redesign

---
+++ src/lib/components/InputAmount.svelte
@@ -0,0 +1,81 @@
+<script>
+	let { coffee = {}, label = '' } = $props();
+
+	const STEP_VALUE = 50;
+</script>
+
+<label for="counter-input" class="block mb-1 text-lg font-medium">{label}</label>
+<div class="relative flex items-center">
+	<button
+		onclick={coffee.decrement(STEP_VALUE)}
+		type="button"
+		id="decrement-button"
+		data-input-counter-decrement="counter-input"
+		class="flex-shrink-0 bg-gray-50 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+	>
+		<svg
+			class="w-4 h-4"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 18 2"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M1 1h16"
+			/>
+		</svg>
+	</button>
+	<input
+		type="number"
+		id="counter-input"
+		data-input-counter
+		class="flex-shrink-0 border-0 bg-transparent font-normal focus:outline-none focus:ring-0 max-w-[5rem] text-center"
+		placeholder=""
+		bind:value={coffee.amount}
+		onchange={(a) => console.log(a)}
+		required
+	/>
+	<span class="mr-2">ml</span>
+	<button
+		onclick={coffee.increment(STEP_VALUE)}
+		type="button"
+		id="increment-button"
+		data-input-counter-increment="counter-input"
+		class="flex-shrink-0 bg-gray-100 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+	>
+		<svg
+			class="w-4 h-4"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 18 18"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 1v16M1 9h16"
+			/>
+		</svg>
+	</button>
+</div>
+
+<style>
+	/*Hide Arrows From Input Number *
+/* Chrome, Safari, Edge, Opera */
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	/* Firefox */
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
-<script>
-	const coffee = $state({ amount: 250 });
+<script lang="ts">
+	import InputAmount from '$lib/components/InputAmount.svelte';
+
+	function createCoffee(amount: number) {
+		let _amount = $state(amount);
+
+		function increment(value: number) {
+			return () => (_amount += value);
+		}
+
+		function decrement(value: number) {
+			return () => (_amount -= value);
+		}
+
+		return {
+			get amount() {
+				return _amount;
+			},
+			set amount(value: number) {
+				_amount = value;
+			},
+			increment,
+			decrement
+		};
+	}
+
+	const coffee = createCoffee(250);
 
 	let coffee_powder = $derived(Math.floor(coffee.amount * 0.08));
 	let hot_warter = $derived(Math.floor(coffee.amount * (3 / 5)));
@@ -9,37 +34,58 @@
 	let hot_water_3 = $derived(Math.floor(hot_warter * (3 / 5)));
 </script>
 
-<h1>アイスコーヒー計算機</h1>
+<div class="container mt-2 ml-10 text-slate-900">
+	<h1
+		class="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"
+	>
+		☕ Coffee Calculator
+	</h1>
 
-<p>何mlのアイスコーヒーを作りますか?</p>
+	<div class="mt-10">
+		<InputAmount {coffee} label={'何mlのアイスコーヒーを作りますか?'} />
+	</div>
 
-<input type="number" bind:value={coffee.amount} /> ml
+	<h2 class="mt-5 mb-2 text-lg font-semibold">材料:</h2>
+	<ul class="ml-2 space-y-1 list-disc list-inside">
+		<li>{coffee_powder} gのコーヒー粉</li>
+		<li>{ice} gの氷</li>
+		<li>{hot_warter} mlのお湯</li>
+	</ul>
 
-<h2>材料:</h2>
+	<h2 class="mt-5 mb-2 text-lg font-semibold">手順:</h2>
 
-<ul>
-	<li>{coffee_powder} gのコーヒー粉</li>
-	<li>{ice} gの氷</li>
-	<li>{hot_warter} mlのお湯</li>
-</ul>
+	<ol class="space-y-4 list-decimal list-inside">
+		<li>
+			準備
+			<ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
+				<li>ドリッパーにペーパをセットしし、湯通ししてペーパー全体を湿らせる（お湯は分量外）</li>
+				<li>ドリップを受ける容器に氷をすべていれる</li>
+				<li>ドリッパーにコーヒー粉を入れ、軽くならす</li>
+			</ol>
+		</li>
+		<li>
+			お湯を注ぐ
+			<ul class="ps-5 mt-2 space-y-1 list-decimal list-inside">
+				<li>お湯{hot_water_1_2}mlを注ぎ、1分蒸らす (真ん中から細くゆっくりと注ぐ)</li>
+				<li>お湯{hot_water_1_2}mlを注ぎ、1分蒸らす</li>
+				<li>残りのお湯{hot_water_3}mlを注ぐ (少し早めに注ぐ)</li>
+			</ul>
+		</li>
+	</ol>
 
-<h2>手順:</h2>
-
-<ul>
-	<li>ドリッパー全体に湯通しし、ペーパー全体を湿らせる（分量外）</li>
-	<li>ドリップを受ける容器に氷をすべていれる</li>
-	<li>ドリッパーにコーヒー粉を入れ、軽くならす</li>
-	<li>1投目: お湯{hot_water_1_2}mlを注ぎ、1分蒸らす *真ん中から細くゆっくりと注ぐ</li>
-	<li>2投目: お湯{hot_water_1_2}mlを注ぎ、1分蒸らす</li>
-	<li>3投目: 残りのお湯{hot_water_3}mlを注ぐ *少し早めに注ぐ</li>
-</ul>
-
-<h2>参考:</h2>
-<ul>
-	<li>
-		<a href="https://www.youtube.com/watch?v=2-VGeC0SFMU">レシピは井崎英典さんの動画より</a>
-	</li>
-	<li>
-		<a href="https://github.com/kj-9/coffee-calculator">ソースコード</a>
-	</li>
-</ul>
+	<h2 class="mt-5 mb-2 text-lg font-semibold">参考:</h2>
+	<ul class="ml-2 space-y-1 list-disc list-inside">
+		<li>
+			<a
+				href="https://www.youtube.com/watch?v=2-VGeC0SFMU"
+				class="font-medium text-blue-600 hover:underline">レシピは井崎英典さんの動画より</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://github.com/kj-9/coffee-calculator"
+				class="font-medium text-blue-600 hover:underline">ソースコード</a
+			>
+		</li>
+	</ul>
+</div>
