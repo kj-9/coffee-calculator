daa80f66d47b81ddb14ed1f7a2d4e7e8ae325d2e
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-01 19:12:20 +0900

    lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,55 +49,57 @@
 			<InputAmount {coffee} label={'何mlのアイスコーヒーを作りますか?'} />
 		</Card>
 	</div>
-	
+
 	<div class="ml-2">
-	<h2 class="mt-10 mb-2 text-lg font-semibold">材料</h2>
-	<ul class="ml-2 space-y-2 list-disc list-inside">
-		<li><Tag>{coffee_powder} g</Tag>のコーヒー粉</li>
-		<li><Tag>{ice} g</Tag>の氷</li>
-		<li><Tag>{hot_warter} ml</Tag>のお湯</li>
-	</ul>
+		<h2 class="mt-10 mb-2 text-lg font-semibold">材料</h2>
+		<ul class="ml-2 space-y-2 list-disc list-inside">
+			<li><Tag>{coffee_powder} g</Tag>のコーヒー粉</li>
+			<li><Tag>{ice} g</Tag>の氷</li>
+			<li><Tag>{hot_warter} ml</Tag>のお湯</li>
+		</ul>
 
-	<h2 class="mt-10 mb-2 text-lg font-semibold">手順</h2>
+		<h2 class="mt-10 mb-2 text-lg font-semibold">手順</h2>
 
-	<ol class="space-y-4 list-decimal list-inside">
-		<li>
-			準備
-			<ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
-				<li>ドリッパーにペーパをセットしし、湯通ししてペーパー全体を湿らせる（お湯は分量外）</li>
-				<li>ドリップを受ける容器に氷をすべていれる</li>
-				<li>ドリッパーにコーヒー粉を入れ、軽くならす</li>
-			</ol>
-		</li>
-		<li>
-			お湯を注ぐ
-			<ul class="ps-5 mt-2 space-y-2 list-decimal list-inside">
-				<li><Tag>{hot_water_1_2} ml</Tag>のお湯を注ぎ、1分蒸らす (真ん中から細くゆっくりと注ぐ)</li>
-				<li><Tag>{hot_water_1_2} ml</Tag>のお湯を注ぎ、1分蒸らす</li>
-				<li><Tag>{hot_water_3} ml</Tag>の残りのお湯を注ぐ (少し早めに注ぐ)</li>
-			</ul>
-		</li>
-	</ol>
+		<ol class="space-y-4 list-decimal list-inside">
+			<li>
+				準備
+				<ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
+					<li>ドリッパーにペーパをセットしし、湯通ししてペーパー全体を湿らせる（お湯は分量外）</li>
+					<li>ドリップを受ける容器に氷をすべていれる</li>
+					<li>ドリッパーにコーヒー粉を入れ、軽くならす</li>
+				</ol>
+			</li>
+			<li>
+				お湯を注ぐ
+				<ul class="ps-5 mt-2 space-y-2 list-decimal list-inside">
+					<li>
+						<Tag>{hot_water_1_2} ml</Tag>のお湯を注ぎ、1分蒸らす (真ん中から細くゆっくりと注ぐ)
+					</li>
+					<li><Tag>{hot_water_1_2} ml</Tag>のお湯を注ぎ、1分蒸らす</li>
+					<li><Tag>{hot_water_3} ml</Tag>の残りのお湯を注ぐ (少し早めに注ぐ)</li>
+				</ul>
+			</li>
+		</ol>
 
-	<div class="mt-8" />
-	<Card>
-		<Timer />
-	</Card>
+		<div class="mt-8"></div>
+		<Card>
+			<Timer />
+		</Card>
 
-	<h2 class="mt-10 mb-2 text-lg font-semibold">参考:</h2>
-	<ul class="ml-2 space-y-1 list-disc list-inside">
-		<li>
-			<a
-				href="https://www.youtube.com/watch?v=2-VGeC0SFMU"
-				class="font-medium text-blue-600 hover:underline">レシピは井崎英典さんの動画より</a
-			>
-		</li>
-		<li>
-			<a
-				href="https://github.com/kj-9/coffee-calculator"
-				class="font-medium text-blue-600 hover:underline">ソースコード</a
-			>
-		</li>
-	</ul>
-</div>
+		<h2 class="mt-10 mb-2 text-lg font-semibold">参考:</h2>
+		<ul class="ml-2 space-y-1 list-disc list-inside">
+			<li>
+				<a
+					href="https://www.youtube.com/watch?v=2-VGeC0SFMU"
+					class="font-medium text-blue-600 hover:underline">レシピは井崎英典さんの動画より</a
+				>
+			</li>
+			<li>
+				<a
+					href="https://github.com/kj-9/coffee-calculator"
+					class="font-medium text-blue-600 hover:underline">ソースコード</a
+				>
+			</li>
+		</ul>
+	</div>
 </div>
