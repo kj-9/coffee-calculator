626a19d329ff2dc16d4a950c1eaeba89552664d8
Kj <46800811+kj-9@users.noreply.github.com>
2024-06-06 09:06:48 +0900

    style: Update Header component with consistent font size and spacing

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,8 @@
 <header>
 	<nav class="bg-brown-900 border-brown-200 px-4 lg:px-6 py-2.5">
-		<div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+		<div class="flex flex-wrap justify-between items-center mx-auto">
 			<div class="flex items-center">
-				<span class="text-2xl font-extrabold mr-3 lg:text-5xl">☕</span>
+				<span class="text-2xl font-extrabold mr-3 lg:text-4xl">☕</span>
 				<span class="text-white self-center text-xl font-semibold whitespace-nowrap"
 					>Coffee Calculator</span
 				>
