90c48b4605d2451d9e89e8720d7b0eca3637bfb4
Kj <46800811+kj-9@users.noreply.github.com>
2024-10-06 18:15:51 +0900

    adding logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,12 @@
+<script>
+	import logo from '$lib/assets/logo.svg';
+</script>
+
 <header>
 	<nav class="bg-brown-900 border-brown-200 px-4 lg:px-6 py-2.5">
 		<div class="flex flex-wrap justify-between items-center mx-auto">
 			<div class="flex items-center">
-				<span class="text-2xl font-extrabold mr-3 lg:text-4xl">☕</span>
+				<img alt="coffee logo" src={logo} class="w-24 mr-2" />
 				<span class="text-white self-center text-xl font-semibold whitespace-nowrap"
 					>Coffee Calculator</span
 				>
